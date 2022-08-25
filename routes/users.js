87397ce020ab85d73d133b35dc8d14e2f09e37a4
Camilla Cortney <camilla.cortney@gmail.com>
2022-08-24 23:46:29 -0700

    users routes

---
+++ routes/users.js
@@ -0,0 +1,125 @@
+"use strict";
+
+/** Routes for users. */
+
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
+const { BadRequestError } = require("../expressError");
+const User = require("../models/user");
+const { createToken } = require("../helpers/tokens");
+const userNewSchema = require("../schemas/userNew.json");
+const userUpdateSchema = require("../schemas/userUpdate.json");
+
+const router = express.Router();
+
+
+/** POST / { user }  => { user, token }
+ *
+ * Adds a new user. This is not the registration endpoint --- instead, this is
+ * only for admin users to add new users. The new user being added can be an
+ * admin.
+ *
+ * This returns the newly created user and an authentication token for them:
+ *  {user: { email, firstName, lastName, profilePicture, isAdmin }, token }
+ *
+ * Authorization required: admin
+ **/
+
+router.post("/", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const user = await User.register(req.body);
+    const token = createToken(user);
+    return res.status(201).json({ user, token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** GET / => { users: [ {email, firstName, lastName, profilePicture, isAdmin} }, ... ] }
+ *
+ * Returns list of all users.
+ *
+ * Authorization required: admin
+ **/
+
+router.get("/", ensureAdmin, async function (req, res, next) {
+  try {
+    const users = await User.findAll();
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** GET /[id] => { user }
+ *
+ * Returns { email, firstName, lastName, isAdmin, buddyReads, buddies, posts }
+ *   where buddyReads is { id, book_id, buddy, status }
+ *   where buddies is { buddyread_id, progress, rating }
+ *   where posts is { id, buddyread_id, page, message, viewed, liked}
+ *
+ * Authorization required: admin or same user-as-:id
+ **/
+
+router.get("/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
+  try {
+    const user = await User.get(req.params.id);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** PATCH /[id] { user } => { user }
+ *
+ * Data can include:
+ *   { firstName, lastName, password, email, profilePicture }
+ *
+ * Returns { firstName, lastName, email, profilePicture, isAdmin }
+ *
+ * Authorization required: admin or same-user-as-:id
+ **/
+
+router.patch("/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const user = await User.update(req.params.id, req.body);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** DELETE /[id]  =>  { deleted: id }
+ *
+ * Authorization required: admin or same-user-as-:id
+ **/
+
+router.delete("/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
+  try {
+    await User.remove(req.params.id);
+    return res.json({ deleted: req.params.id });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
